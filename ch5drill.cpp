6470ba9bfea3df9630ec7ad09ab5d1f4dbcf8743
gyorfi-daniel <43523045+gyorfi-daniel@users.noreply.github.com>
2018-11-13 16:26:17 +0100

    Add files via upload

---
+++ ch5drill.cpp
@@ -0,0 +1,55 @@
+#include "std_lib_facilities.h"
+int main()
+{
+
+//cout << "Success!\n";  //Hibás: Cout << "Success!\n";
+
+//cout << "Success!\n"; //Hibás: cout << "Success!\n;
+
+//cout << "Success" << "!\n" //Hibás:scout << "Success" << !\n"
+
+//cout << "Success" << '\n';  //Hibás:cout << success << '\n'
+
+//int res = 7; vector<int> v(10); v[5] = res; cout << "Success!\n"; //string -> int 
+
+//vector<int> v(10); v(5) = 7; if (v[5]!=7) cout << "Success!\n"; //v[5] a v(5) helyett
+
+//if (1 = 1) cout << "Success!\n"; else cout << "Fail!\n";
+
+//bool c = false; if (!c) cout << "Success!\n"; else cout << "Fail!\n";
+
+//string s = "ape"; bool c = "fool" > s; if (c) cout << "Success!\n";
+
+//string s = "ape"; if (s!="fool") cout << "Success!\n";
+
+//string s = "ape"; if (s!="fool") cout << "Success!\n";
+
+//string s = "ape"; if (s < "fool") cout << "Success!\n";
+
+//vector<char> v(5); for (int i=0; i < v.size(); ++i) ; cout << "Success!\n";
+
+//vector<char> v(5); for (int i=0; i<=v.size(); ++i) ; cout << "Success!\n";
+
+//string s = "Success!\n"; for (int i=0; i<s.size(); ++i) cout << s[i];
+
+//if (true) cout << "Success!\n"; else cout << "Fail!\n";
+
+//int x = 2000; char c = x; if (c==2000) cout << "Success!\n";
+
+//string s = "Success!\n"; for (int i=0; i<s.size(); ++i) cout << s[i];
+
+//vector v(5); for (int i=0; i<=v.size(); ++i) ; cout << "Success!\n";
+
+//int i=0; int j = 9; while (i<10) ++i; if (j<i) cout << "Success!\n";
+
+//double x = 2.0; double d = 5.0/(x+2.0); if (d < (2.0*x+0.5)) cout << "Success!\n";
+
+//string s = "Success!\n"; for (int i=0; i<s.size(); ++i) cout << s[i];
+
+//int i = 0; while (i<10) ++i; int j = 0; if (j<i) cout << "Success!\n";
+
+//double x = 4.0; double d = 5.0/(x–2.0); if (d == (5.0/x-2.0)) cout << "Success!\n";
+
+//cout << "Success!\n";
+
+}
